677977a7d535537471855eaa30d0ef896415e508
zirus23 <skalband23@gmail.com>
2024-05-13 20:47:59 -0700

    fix Prompts.svelte

---
+++ src/lib/Prompts.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+
+  const inputPrompt = (prompt: string) => {
+    input.value = prompt
+    input.style.height = 'auto'
+    input.style.height = input.scrollHeight + 'px'
+  }
+
+  export let input : HTMLTextAreaElement
+</script>
